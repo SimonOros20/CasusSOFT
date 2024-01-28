1b3c20f69b639c6bea7cff088b0285f2c9850d87
Metalhenk <117105109+Metalhenk@users.noreply.github.com>
2024-01-28 18:43:54 +0100

    Add files via upload

---
+++ CasusSOFT/triggers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus_Blok_5__versie_2_
+{
+    internal class triggers
+    {
+    }
+}
